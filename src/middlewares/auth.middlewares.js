5f0b4642d9ed406dd44172c22ce486362d7b0709
mohit-Byte-Roamer <mohit878421@gmail.com>
2024-09-21 19:53:39 +0530

    correction in auth-middlewares

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -13,31 +13,33 @@ export const AuthMiddleWares = () => {
 // and last assign user in req.user and call next function
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
-  console.log("MOHIT");
-    
+    console.log("MOHIT");
+
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+      return res.status(401).json(new ApiError(401, "Unauthorized request"));
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const User = await user
       .findById(decodedToken._id)
       .select("-password,-refreshToken");
     if (!User) {
-      throw new ApiError(401, "Invalid Access Token");
+      return res.status(401).json(new ApiError(401, "Invalid Access Token"));
     }
     req.user = User;
     next();
   } catch (error) {
-    throw new ApiError(402, error?.message || "Invalid access token");
+    return res
+      .status(402)
+      .json(new ApiError(402, error?.message || "Invalid access token"));
   }
 });
 
 export const isAdmin = (req, _, next) => {
   console.log("ISADMIN");
-  
+
   if (req.user.role !== "admin") {
     throw new ApiError(403, "Permission denied. User is not an admin");
   }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,7 +26,6 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
-router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
