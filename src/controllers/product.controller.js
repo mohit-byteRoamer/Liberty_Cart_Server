c6466415aef6a10f95f3928658b60db01f4dd473
mohit-Byte-Roamer <mohit878421@gmail.com>
2024-11-09 12:23:39 +0530

    implement limit for get-products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -160,8 +160,10 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 const getAllProducts = asyncHandler(async (req, res) => {
   const { search, sort, category, price } = req.query;
+  console.log();
+  
   const page = Number(req.query.page) || 1;
-  const limit = Number(process.env.PRODUCT_PER_PAGE) || 10;
+  const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
   const baseQuery = {};
